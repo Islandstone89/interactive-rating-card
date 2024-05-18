bbadad4fd9c104d8907cbc0a5c76c4ce9489b692
Øystein Håberg <islandstone89@gmail.com>
2024-05-19 00:01:55 +0200

    Selected the cards and the form in JS * Experimented with radio button styling

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+/* Select the rating card */
+let ratingCard = document.querySelector(".rating-card");
+
+/* Select the rating card */
+let confirmationCard = document.querySelector(".confirmation-card");
+
+/* Write function that gets triggered on form submission */
+function confirmation(e) {
+    e.preventDefault()
+    ratingCard.classList.toggle("hide");
+    confirmationCard.classList.toggle("show");
+}
+
+/* Select the form */
+let form = document.querySelector(".form");
+
+form.addEventListener("submit", confirmation);
