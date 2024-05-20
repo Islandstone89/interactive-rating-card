0c9d1d1da34edb890b8fcdd9b6cc59f359024827
Øystein Håberg <islandstone89@gmail.com>
2024-05-21 01:15:05 +0200

    * Added hide class on confirmation-card by default * Adjusted spacing values

--- script.js
+++ script.js
@@ -9,10 +9,24 @@ let confirmationCard = document.querySelector(".confirmation-card");
 function confirmation(e) {
     e.preventDefault()
     ratingCard.classList.toggle("hide");
-    confirmationCard.classList.toggle("show");
+    confirmationCard.classList.toggle("hide");
+    /* selectionMessage.textContent = `You selected ${} out of ${}`; */
 }
 
 /* Select the form */
 let form = document.querySelector(".form");
 
 form.addEventListener("submit", confirmation);
+
+/* Select the radio buttons */
+
+let radioButtons = document.querySelectorAll(".rating-test");
+
+
+
+function getValue(e) {
+    let value = e.target.value;
+}
+
+/* Select the selection paragraph */
+let selectionMessage = document.querySelector(".confirmation-card__selection");
